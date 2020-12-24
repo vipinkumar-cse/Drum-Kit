1a6387ee9be541e7de91abfcbaf80c0e11bb18a7
vipinkumar-cse <70096266+vipinkumar-cse@users.noreply.github.com>
2020-12-24 13:11:39 +0530

    added sounds to the website
    Website completed

---
+++ index.js
@@ -0,0 +1,52 @@
+var numberofbuttons = document.querySelectorAll(".drum").length;
+for(var i=0;i<numberofbuttons;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",play);
+
+
+}
+
+function play(){
+  soundselect(this.textContent);
+
+
+
+}
+
+//function to detec keyboard
+
+document.addEventListener("keypress", function (event){
+  // console.log(event.key);
+  soundselect(event.key);
+})
+
+//function for switch case sounds
+function soundselect(value)
+{
+  this.a=value;
+  switch(this.a)
+  {
+    case 'w' : var audio = new Audio("sounds/tom-1.mp3")
+    audio.play();
+    break;
+    case 'a' :   var audio = new Audio("sounds/tom-2.mp3")
+      audio.play();
+      break;
+
+      case 's' :   var audio = new Audio("sounds/tom-3.mp3")
+        audio.play();
+        break;
+        case 'd' :   var audio = new Audio("sounds/tom-4.mp3")
+          audio.play();
+          break;
+          case 'j' :   var audio = new Audio("sounds/snare.mp3")
+            audio.play();
+            break;
+            case 'k' :   var audio = new Audio("sounds/crash.mp3")
+              audio.play();
+              break;
+              case 'l' :   var audio = new Audio("sounds/kick-bass.mp3")
+                audio.play();
+                break;
+              }
+
+}
